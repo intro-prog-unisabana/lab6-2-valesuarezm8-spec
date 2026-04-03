0059d741b7a496671bd3975d39264868d7c09106
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-04-03 18:08:14 -0500

    4

--- dict_store.py
+++ dict_store.py
@@ -1,5 +1,8 @@
-
+datos = {"temp": 22.5, "color": "blue", "status": "ok"}
 def temp_and_color(datos):
     temp=datos.get("temp")
-    color=datos.get("blue")
+    color=datos.get("color")
     return (temp, color)
+print(temp_and_color(datos))
+datos ={"status": "ok"}
+print(temp_and_color(datos))
