d6b413ebf477553fa07c92a64b5563663dda6475
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-04-03 15:58:19 -0500

    1

--- dict_concept.py
+++ dict_concept.py
@@ -1,1 +1,7 @@
-# Write your code here!
+def get_config():
+    return {
+        "width" :int(480),
+        "height":int(480),
+        "color_mode":str("dark"),
+        "sensitivity":float(0.4)
+        }
